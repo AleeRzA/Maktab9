7a2aabf576da7a9e551fc63fa6c1d49bcb6c51e9
alirezanasr <alireza_nasrollahi@yahoo.com>
2019-01-26 23:04:44 +0330

    creating the basics

---
+++ app/src/main/java/com/example/android/musicplayer/controller/MusicListActivity.java
@@ -0,0 +1,12 @@
+package com.example.android.musicplayer.controller;
+
+import android.support.v4.app.Fragment;
+
+public class MusicListActivity extends SingleFragmentActivity {
+
+
+    @Override
+    public Fragment getFragment() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/android/musicplayer/controller/SingleFragmentActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.musicplayer.controller;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.musicplayer.R;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    public abstract Fragment getFragment();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_list);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+//        if(fragmentManager.findFragmentById(R.id.))
+        fragmentManager.beginTransaction()
+//                       .add()
+                       .commit();
+    }
+}
